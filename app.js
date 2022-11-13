31bee0455237f9b7886434b486099030850b96a8
thang pham <thangpham1117@gmail.com>
2022-11-13 16:01:48 +0700

    add first cm

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const axios = require('axios')
-var bodyParser = require('body-parser')
+const express = require("express");
+const axios = require("axios");
+var bodyParser = require("body-parser");
 
-const app = express()
-const port = 80
-const baseUrl= 'https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get'
+const app = express();
+const baseUrl =
+  "https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get";
 
-app.use(bodyParser.json())
-app.get('/', async (req, res) => {
-  res.send("hello world")
-})
-app.get('/portal', async (req, res) => {
+app.use(bodyParser.json());
+app.get("/", async (req, res) => {
+  res.send("hello world");
+});
+app.get("/portal", async (req, res) => {
   try {
-    const {FromDate,ToDate,EmployeeID} = req.body;
-    const resp= await axios.get(`${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`)
-    if(resp && resp?.data){
-      res.send({portal : resp.data})
+    const { FromDate, ToDate, EmployeeID } = req.body;
+    const resp = await axios.get(
+      `${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`
+    );
+    if (resp && resp?.data) {
+      res.send({ portal: resp.data });
     }
   } catch (error) {
-    res.status(500)
-    res.send({message: "NGU"})
+    res.status(500);
+    res.send({ message: "NGU" });
   }
-})
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 3000)
