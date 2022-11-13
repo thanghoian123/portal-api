e151edb3b0ec496f42ec2671f26b1592eff63f06
Rabb <rabb.pham@nfcguest.com>
2022-11-13 11:56:59 +0700

    update by required comment on PR

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const axios = require('axios')
+var bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
+const baseUrl= 'https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(bodyParser.json())
+app.get('/', async (req, res) => {
+  
+  req.headers['ngrok-skip-browser-warning'] = '1234'
+  res.send("hello world")
+})
+app.get('/portal', async (req, res) => {
+  try {
+    const {FromDate,ToDate,EmployeeID} = req.body;
+    const resp= await axios.get(`${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`)
+    if(resp && resp?.data){
+      res.send({portal : resp.data})
+    }
+  } catch (error) {
+    res.status(500)
+    res.send({message: "NGU"})
+  }
 })
 
 app.listen(port, () => {
