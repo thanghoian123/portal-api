c241d463266813d3ca4af099fb9d0fefdb413550
thang pham <thangpham1117@gmail.com>
2022-11-13 16:39:12 +0700

    add first cm

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 const express = require("express");
-const axios = require("axios");
+const portalRoute = require("./portalRoute");
 var bodyParser = require("body-parser");
 
 const app = express();
-const baseUrl =
-  "https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get";
-
 app.use(bodyParser.json());
-app.get("/", async (req, res) => {
-  res.send("hello world");
-});
-app.get("/portal", async (req, res) => {
-  try {
-    const { FromDate, ToDate, EmployeeID } = req.body;
-    const resp = await axios.get(
-      `${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`
-    );
-    if (resp && resp?.data) {
-      res.send({ portal: resp.data });
-    }
-  } catch (error) {
-    res.status(500);
-    res.send({ message: "NGU" });
-  }
-});
 
-app.listen(process.env.PORT || 3000)
+app.use("/", portalRoute);
+
+app.listen(process.env.PORT || 3000);
---
+++ portalRoute.js
@@ -0,0 +1,28 @@
+const portalRoute = require("express").Router();
+var bodyParser = require("body-parser");
+portalRoute.use(bodyParser.json())
+const axios = require("axios");
+const baseUrl =
+  "https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get";
+
+portalRoute.get("/portal", async (req, res) => {
+  console.log("-------- portal");
+  try {
+    const { FromDate, ToDate, EmployeeID } = req.body;
+    const resp = await axios.get(
+      `${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`
+    );
+    if (resp && resp?.data) {
+      res.send({ portal: resp.data });
+    }
+  } catch (error) {
+    res.status(500);
+    res.send({ message: "NGU" });
+  }
+});
+
+portalRoute.get("/", async (req, res) => {
+  console.log("-------- haha");
+  res.send("hello");
+});
+module.exports = portalRoute;
