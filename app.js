f1cbb08aa1baa35996b51e5e181741062c1ceed5
Rabb <rabb.pham@nfcguest.com>
2022-11-13 12:11:27 +0700

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 var bodyParser = require('body-parser')
 
 const app = express()
-const port = 3000
+const port = 80
 const baseUrl= 'https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get'
 
 app.use(bodyParser.json())
