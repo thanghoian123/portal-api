1c1824f8132fc2e688a1ba8ff1c9c075950edd56
Rabb <rabb.pham@nfcguest.com>
2022-11-14 16:54:08 +0700

    change filter report withdrawn children

--- portalRoute.js
+++ portalRoute.js
@@ -7,19 +7,23 @@ const baseUrl =
 
 portalRoute.get("/portal", async (req, res) => {
   console.log("-------- portal");
-  res.send("portal")
-  // try {
-  //   const { FromDate, ToDate, EmployeeID } = req.body;
-  //   const resp = await axios.get(
-  //     `${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`
-  //   );
-  //   if (resp && resp?.data) {
-  //     res.send({ portal: resp.data });
-  //   }
-  // } catch (error) {
-  //   res.status(500);
-  //   res.send({ message: "NGU" });
-  // }
+  try {
+    const { FromDate, ToDate, EmployeeID } = req.body;
+    if(FromDate&&ToDate&&EmployeeID){
+      const resp = await axios.get(
+        `${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`
+      );
+      if (resp && resp?.data) {
+        res.send({ portal: resp.data });
+      }
+    }else{
+      res.send("NGU")
+    }
+   
+  } catch (error) {
+    // res.status(500);
+    res.send({ message: "NGU" });
+  }
 });
 
 portalRoute.get("/", async (req, res) => {
