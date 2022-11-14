69f2adc136bd7ee0ab7ed8d4089e24b3df5c0a73
Rabb <rabb.pham@nfcguest.com>
2022-11-14 16:57:44 +0700

    change filter report withdrawn children

--- portalRoute.js
+++ portalRoute.js
@@ -6,14 +6,17 @@ const baseUrl =
   "https://portaladmin.orientsoftware.net/api/EmployeeTimeSheet/get";
 
 portalRoute.get("/portal", async (req, res) => {
-  console.log("-------- portal");
   try {
     const { FromDate, ToDate, EmployeeID } = req.body;
     if(FromDate&&ToDate&&EmployeeID){
+      console.log("-------- 1");
       const resp = await axios.get(
         `${baseUrl}?FromDate=${FromDate}&ToDate=${ToDate}&EmployeeID=${EmployeeID}`
       );
+      console.log("-------- 2");
+
       if (resp && resp?.data) {
+        console.log("-------- 3");
         res.send({ portal: resp.data });
       }
     }else{
